7b0d45d2831fec94220016761274a43db6a87b22
dfrafelipe30 <33558046+dfrafelipe30@users.noreply.github.com>
2018-04-06 15:04:21 -0500

    Update PilaStack.cpp

--- PilaStack/src/PilaStack.cpp
+++ PilaStack/src/PilaStack.cpp
@@ -5,11 +5,25 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include "clase_pila_stack.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+	Pila arreglo;
+	cout << "bien"<< endl ;
+
+	for (int i = 0;i < 30;i++)
+	{
+		arreglo.Push('A'+ i);
+	}
+
+	cout <<"---------------"<< endl;
+
+	for(int j = 0; j < 30;j++)
+	{
+		cout << arreglo.Pop() << endl ;
+	}
+	cout << endl;
+
 }
